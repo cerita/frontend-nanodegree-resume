92696a81d764bc1d6174988e0385a5fd3019ac3e
Cerita <ceritakelly@gmail.com>
2016-07-16 13:23:29 -0500

    JSON
    Updated files from Data Types section in course. Uses JSON for work,
    school, projects, & bio sections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,99 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Cerita Kelly");
+var role = "Web Developer"
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+
+
+
+var bio = {
+    "name" : "Cerita",
+    "role" : "Web Developer",
+    "contact" : {
+        "email" : "ceritakelly@gmail.com",
+        "mobile" : "773-349-1918"   ,
+        "github" : "cerita",
+        "location" : "Austin"
+    },
+    "welcomeMsg" : "Hello world!",
+    "skills" : ['awesomeness', 'sarcasm', 'potato'],
+    "bioPic" : "images/fry.jpg"
+};
+
+$("#main").append(bio);
+
+
+var projects = {
+    "project": [
+        {
+            "title":"",
+            "date":"",
+            "desc":"",
+            "images": [""]
+        }
+    ]
+}
+
+var work = {
+    "jobs": [{
+        "title": "Web Coordinator",
+        "employer": "",
+        "dates": "",
+        "location": "Austin, TX",
+        "desc": "Stuff and things"
+    }, {
+        "title": "Technology Support Specialist",
+        "employer": "CCSD 146",
+        "dates": "2012-2015",
+        "location": "Chicago, IL",
+        "desc": "Stuff and things"
+    }, {
+        "title": "Web Coordinator",
+        "employer": "",
+        "dates": "",
+        "location": "Austin, TX",
+        "desc": "Stuff and things"
+    }]
+}
+    
+
+var education = {
+    "schools": [
+        {
+            "name":"Saint Xavier University",
+            "city":"Chicago, IL, US",
+            "degree":"BA",
+            "major":"Computer Science"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "JavaScript Basics",
+            "school":"Udacity",
+            "dates":"2016",
+            "url":"http://udacity.com"
+        }
+    ]
+};
+
+
+
+$("#main").append(work["position"]);
+$("#main").append(education.school);
+
+
+
+
+
+
+
+
+
+
+
+
+
